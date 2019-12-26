a21cf81fc8542587a53dd438f2a032c6ec880d32
Yulia <yulia.kotseruba@gmail.com>
2019-12-26 10:25:53 -0500

    Update download_pretrained_weights.sh

--- models/download_pretrained_weights.sh
+++ models/download_pretrained_weights.sh
@@ -1,2 +1,2 @@
 #download pretrained SALICONtf
-wget https://dl.dropboxusercontent.com/s/56gvsc8a8eskewt/model_lr0.01_loss_crossentropy.h5
+wget http://data.nvision2.eecs.yorku.ca/P3O3/data/model_lr0.01_loss_crossentropy.h5
